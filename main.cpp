d65b261e36936823395652d2dde271f6431f0cc9
8R1Xee4 <saturday020905@gmail.com>
2025-07-07 09:09:22 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    // 1) Create the QApplication instance
+    QApplication app(argc, argv);
+
+    // 2) Instantiate and show your MainWindow
+    MainWindow w;
+    w.show();
+
+    // 3) Enter the Qt event loop
+    return app.exec();
+}
